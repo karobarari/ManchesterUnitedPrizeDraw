bad7e79afbbaa8e236260b406a0ac0685221c38e
Karo <karobarari@gmail.com>
2026-05-19 10:36:21 +0100

    feat: add profile section with routes for Profile, Subscriptions, and Payments

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,23 @@
 import { Routes, Route } from 'react-router-dom'
-import Layout from './components/Layout'
-import Home from './pages/Home'
+import Layout        from './components/Layout'
+import Home          from './pages/Home'
+import Account       from './pages/account/Account'
+import Profile from './pages/account/Profile'
+import Subscriptions from './pages/account/Subscriptions'
+import Payments      from './pages/account/Payments'
 
 export default function App() {
   return (
     <Routes>
       <Route element={<Layout />}>
         <Route path="/" element={<Home />} />
-        {/* add more pages here as you build them */}
+
+        {/* Profile section */}
+        <Route path="/profile" element={<Account />}>
+          <Route index                element={<Profile />} />
+          <Route path="subscriptions" element={<Subscriptions />} />
+          <Route path="payments"      element={<Payments />} />
+        </Route>
       </Route>
     </Routes>
   )
